4ad9310640b41fa3c95ae77630bea7354bb8d8af
Jaime2357 <jaimeelepano2357@gmail.com>
2024-03-12 15:04:15 -0700

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './pages/HomePage';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+      initialRouteName = "Home"
+      screenOptions={styles.header} 
+      >
+        <Stack.Screen name = "Home" component ={HomeScreen} options = {{title: 'Dashboard'}}  />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header:{ 
+    headerStyle: {
+      backgroundColor: '#588CD1'
+    }
+  },
+  texttest: {
+    color: 'white',
   },
 });
